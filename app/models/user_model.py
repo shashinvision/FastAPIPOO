a03f434a159702f48b1ed03c091084d24831e1d2
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-04-18 08:50:51 -0400

    UPDATE model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,4 +1,14 @@
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from app.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
 
 
 class UserCreate(BaseModel):
--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -1,14 +1,5 @@
-from sqlalchemy import Column, Integer, String
-from app.db.database import Base, SessionLocal
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-
+from app.models.user_model import User
+from app.db.database import SessionLocal
 
 # Repository functions using SessionLocal directly
 
